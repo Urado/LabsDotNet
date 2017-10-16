cc540cb04b17cca91f6e210754e407fde6afbbcc
Pavel Kalionov <Yrado94@hotmail.com>
2017-10-16 17:01:03 +0300

    begin

---
+++ MyListLab/MyListLab/MyList.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyListLab
+{
+    public class MyList<T> : IList<T>
+    {
+        private MyElement<T> _firstElement = null;
+
+        private MyElement<T> getElementByNumber(int n)
+        {
+            MyElement<T> ret = _firstElement;
+            for (int i = 0; i < n; i++)
+            {
+                if (ret == null)
+                    break;
+                ret = ret.Next;
+            }
+            return ret;
+        }
+
+        public T this[int index] { get { return getElementByNumber(index).Item; } set { getElementByNumber(index).Item = value; } }
+
+        public int Count
+        {
+            get
+            {
+                int ret = 0;
+                for (MyElement<T> i = _firstElement; i != null; i = i.Next)
+                    ret++;
+                return ret;
+            }
+        }
+
+        public bool IsReadOnly{ get { return false; } }
+
+        public void Add(T item)
+        {
+            if (_firstElement != null)
+            {
+                MyElement<T> i;
+                for (i = _firstElement; i.Next != null; i = i.Next) ;
+                i.Next = new MyElement<T>(item);
+            }
+            else
+                _firstElement = new MyElement<T>(item);
+        }
+
+        public void Clear()
+        {
+            _firstElement = null;
+        }
+
+        public bool Contains(T item)
+        {
+            var i = _firstElement;
+            for (; i != null && !i.Item.Equals(item); i = i.Next);
+            return i != null;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            var Elem = _firstElement;
+            for(int i=arrayIndex;Elem!=null;i++, Elem=Elem.Next)
+            {
+                array[i] = Elem.Item;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (MyElement<T> i = _firstElement; i != null; i = i.Next)
+                yield return i.Item;
+        }
+
+        public int IndexOf(T item)
+        {
+            var i = _firstElement;
+            int ret = 0;
+            for (; i != null && !i.Item.Equals(item); i = i.Next)
+                ret++;
+            if (i == null)
+                ret = -1;
+            return ret;
+        }
+
+        public void Insert(int index, T item)
+        {
+            var inElem = new MyElement<T>(item);
+            if (index == 0)
+            {
+                inElem.Next = _firstElement;
+                _firstElement = inElem;
+            }
+            else
+            {
+                var before = getElementByNumber(index - 1);
+                inElem.Next = before.Next;
+                before.Next = inElem;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if(index!=-1)
+                RemoveAt(IndexOf(item));
+            return index != -1;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index > 0)
+            {
+                var before = getElementByNumber(index - 1);
+                if (before != null&&before.Next!=null)
+                    before.Next = before.Next.Next;
+            }
+            else if(index==0)
+                if (_firstElement != null)
+                    _firstElement=_firstElement.Next;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (MyElement<T> i = _firstElement; i != null; i = i.Next)
+                yield return i.Item;
+        }
+
+        public string ToCheckString()
+        {
+            string ret="";
+
+            for (var iter = _firstElement; iter != null; iter = iter.Next)
+                ret += iter.Item.ToString() + " ";
+
+            return ret;
+        }
+
+        internal class MyElement<E>
+        {
+            internal MyElement(E item)
+            {
+                Item = item;
+            }
+            internal E Item { get; set; }
+            internal MyElement<E> Next { get; set; }     
+
+        }
+
+    }
+}
---
+++ MyListLab/UnitTestMyList/UnitTest1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyListLab;
+
+namespace UnitTestMyList
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BaseFunctionTest()
+        {
+            var myListInt = new MyList<int>();
+
+            Assert.IsTrue(myListInt.Count == 0);
+
+            myListInt.Add(0);
+            Assert.IsTrue(myListInt[0] == 0);
+
+            Assert.IsTrue(myListInt.Count == 1);
+
+
+            myListInt.Add(1);
+            Assert.IsTrue(myListInt[0] == 0);
+            Assert.IsTrue(myListInt[1] == 1);
+
+            Assert.IsTrue(myListInt.Count == 2);
+
+            myListInt.Add(2);
+            Assert.IsTrue(myListInt[0] == 0);
+            Assert.IsTrue(myListInt[1] == 1);
+            Assert.IsTrue(myListInt[2] == 2);
+
+            Assert.IsTrue(myListInt.Count == 3);
+
+
+            myListInt.Clear();
+
+            Assert.IsTrue(myListInt.Count == 0);
+
+            myListInt.Add(4);
+            Assert.IsTrue(myListInt[0] == 4);
+
+            Assert.IsTrue(myListInt.Count == 1);
+
+
+            myListInt.Add(5);
+            Assert.IsTrue(myListInt[0] == 4);
+            Assert.IsTrue(myListInt[1] == 5);
+
+            Assert.IsTrue(myListInt.Count == 2);
+
+            myListInt.Add(6);
+            Assert.IsTrue(myListInt[0] == 4);
+            Assert.IsTrue(myListInt[1] == 5);
+            Assert.IsTrue(myListInt[2] == 6);
+        }
+
+        [TestMethod]
+        public void IndexOfTest()
+        {
+            var myListInt = new MyList<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Assert.IsTrue("1 2 3 4 5 6 7 8 9 10 " == myListInt.ToCheckString());
+
+            Assert.IsTrue(myListInt.IndexOf(1) == 0);
+            Assert.IsTrue(myListInt.IndexOf(10) == 9);
+            Assert.IsTrue(myListInt.IndexOf(2) == 1);
+
+            myListInt.Add(11);
+
+            Assert.IsTrue(myListInt.IndexOf(1) == 0);
+            Assert.IsTrue(myListInt.IndexOf(11) == 10);
+            Assert.IsTrue(myListInt.IndexOf(10) == 9);
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            var myListInt = new MyList<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Assert.IsTrue("1 2 3 4 5 6 7 8 9 10 " == myListInt.ToCheckString());
+
+            Assert.IsTrue(myListInt.Contains(1));
+            Assert.IsTrue(myListInt.Contains(5));
+            Assert.IsTrue(myListInt.Contains(10));
+            Assert.IsTrue(!myListInt.Contains(0));
+
+            myListInt.RemoveAt(1);
+            Assert.IsTrue(!myListInt.Contains(2));
+            Assert.IsTrue("1 3 4 5 6 7 8 9 10 " == myListInt.ToCheckString());
+
+            myListInt.RemoveAt(0);
+            Assert.IsTrue(!myListInt.Contains(1));
+            Assert.IsTrue("3 4 5 6 7 8 9 10 " == myListInt.ToCheckString());
+
+            myListInt.RemoveAt(7);
+            Assert.IsTrue(!myListInt.Contains(10));
+            Assert.IsTrue("3 4 5 6 7 8 9 " == myListInt.ToCheckString());
+
+            myListInt.RemoveAt(7);
+            Assert.IsTrue(!myListInt.Contains(10));
+            Assert.IsTrue("3 4 5 6 7 8 9 " == myListInt.ToCheckString());
+
+            myListInt.RemoveAt(-1);
+            Assert.IsTrue(!myListInt.Contains(10));
+            Assert.IsTrue("3 4 5 6 7 8 9 " == myListInt.ToCheckString());
+
+            myListInt.Remove(4);
+            Assert.IsTrue(!myListInt.Contains(4));
+            Assert.IsTrue("3 5 6 7 8 9 " == myListInt.ToCheckString());
+
+            myListInt.Remove(3);
+            Assert.IsTrue(!myListInt.Contains(3));
+            Assert.IsTrue("5 6 7 8 9 " == myListInt.ToCheckString());
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            var myListInt = new MyList<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            myListInt.Insert(10, 11);
+            myListInt.Insert(0, 0);
+            myListInt.Insert(5, 30);
+            Assert.IsTrue("0 1 2 3 4 30 5 6 7 8 9 10 11 " == myListInt.ToCheckString());
+
+        }
+
+        [TestMethod]
+        public void EnumeratorTest()
+        {
+            var myListInt = new MyList<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            string listString = "";
+            foreach(var i in myListInt)
+            {
+                listString += i.ToString() + " ";
+            }
+            Assert.IsTrue("1 2 3 4 5 6 7 8 9 10 " == listString);
+
+            myListInt.Clear();
+            listString = "";
+
+            foreach(var i in myListInt)
+            {
+                listString += i.ToString() + " ";
+            }
+
+            Assert.IsTrue("" == listString);
+        }
+    }
+}
